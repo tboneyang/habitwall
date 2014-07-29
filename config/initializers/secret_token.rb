f2c5278946723a8d69238fa4f0cc642f0de3869d
tboneyang <tboenyang@gmail.com>
2014-07-29 13:28:02 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Habitwall::Application.config.secret_key_base = 'c232ec5c0e2f5788754acae7c7571d628efaaf4fc874ac27e8f20a2f563fb7e8107c0a73ab64cb7fdbeccb2995f2dc869b0d4368e6d3d14d4fe2992ee6f5fd72'
